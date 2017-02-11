8f987c9418b097b3d3260c682f8d241cb56cef38
BrandonDonahue96 <bad68@pitt.edu>
2017-02-11 13:09:51 -0500

    Add files via upload

--- ideaStormer.js
+++ ideaStormer.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
-	alert("working");
+	$('#firstNode').position({of: $(window)});
+	$('.Node').draggable();
 	
-	$('#Node').click(function(){
-		alert("Hello");
+	$('button').click(function(){
+		
+		$('.Node').after('<div class="Node"></div>');
+		$('.Node').draggable();
+		
+		
+		
 	});
+	
+	
 	
 	var input;
 	var delim = [' ', '+', '(', ')', '*', '\\/', ':', '?', '-'];
@@ -12,17 +20,15 @@ $(document).ready(function(){
 		input = document.getElementById('query-Input').value;
 		token = input.split(new RegExp('[' + delim.join('') + ']', 'g'));
 		var querySum = [];
-		for(i=0;i<tokens.length;i++){
-			querySum += tokens[i]
-			if(i!=tokens.length-1){
+		for(i=0;i<token.length;i++){
+			querySum += token[i]
+			if(i!=token.length-1){
 				querySum += '+'
 			}
 		}
-		var originalQuery = "https://www.googleapis.com/customsearch/v1?q=" + querySum + "&tbm=nws&cx=010166564119629920893:idog8_peody";
+		var originalQuery = 'https://cse.google.com/cse.js?cx=010166564119629920893:idog8_peody';
 		$.get(originalQuery, function(data){
-			$('centerNode a').attr('href', data.items[0].link);
-			$('centerNode img').attr('src', data.items[0].pagemap.cse_image[0].src);
-			$('centerNode h3').html(data.items[0].title);
+			$('#firstNode p').html(data.items[0].snippet);
 		});
 		
 	}});
